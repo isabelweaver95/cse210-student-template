40ac9dd3df8c443ca57667350e1fa938d9174973
isabelweaver95 <izzyweav@gmail.com>
2024-01-13 14:53:08 -0700

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char letter;
+        string grade;
+
+        Console.WriteLine("What is your grade percentage?");
+        grade = Console.ReadLine();
+
+        int gradePer = Convert.ToInt32(grade);
+
+        if(gradePer >= 90){
+            letter = 'A';
+        }else if(gradePer < 90 && gradePer >= 80){
+            letter = 'B';
+        }else if(gradePer < 80 && gradePer >= 70){
+            letter = 'C';
+        }else if(gradePer < 70 && gradePer >= 60){
+            letter = 'D';
+        }else{
+            letter = 'F';
+        }
+
+        Console.WriteLine("Your grade is an " + letter);
+        if(gradePer >= 70){
+            Console.WriteLine("Great Work!");
+        }else {
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
