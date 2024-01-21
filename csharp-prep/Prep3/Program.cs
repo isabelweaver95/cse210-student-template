c05a13f44ab7f5a93df55b5456ed12362b029f60
isabelweaver95 <izzyweav@gmail.com>
2024-01-20 17:43:40 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Boolean isValue = true;
+        Console.Write("What is the magic Number? ");
+        string magic = Console.ReadLine();
+        int magicNumber = Convert.ToInt32(magic);
+
+        while(isValue){
+            Console.Write("What is your guess? ");
+            string guessed = Console.ReadLine();
+            int guess = Convert.ToInt32(guessed);
+            if(guess < magicNumber){
+                Console.WriteLine("Higher");
+            }else if (guess > magicNumber){
+                Console.WriteLine("Lower");
+            }else{
+                Console.WriteLine("You Guessed it!");
+                isValue = false;
+            }
+
+        }
     }
 }
