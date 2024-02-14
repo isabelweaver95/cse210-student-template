6d6f8e4050496f672da11f03995b58bd92154cca
isabelweaver95 <izzyweav@gmail.com>
2024-02-13 20:18:44 -0700

    added Referance class

---
+++ prove/Develop03/Referance.cs
@@ -0,0 +1,34 @@
+public class Referance
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+
+    public Referance(){
+
+    }
+
+    public void SetBook(string book){
+        _book = book;
+    }
+
+    public string GetBook(){
+        return _book;
+    }
+
+    public void SetChapter(int chapter){
+        _chapter = chapter;
+    }
+
+    public int GetChapter(){
+        return _chapter;
+    }
+
+    public void SetVerse(int verse){
+        _verse = verse;
+    }
+
+    public int GetVerse(){
+        return _verse;
+    }
+}
