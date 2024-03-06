bac7001e191fccfb3923df761ba198314d835d8a
isabelweaver95 <izzyweav@gmail.com>
2024-03-05 18:49:12 -0700

    finished learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("red", 4);
+        Rectangle rectangle = new Rectangle("blue", 4, 5);
+        Circle circle = new Circle("yellow", 4);
+        Rectangle rectangle1 = new Rectangle("green", 4, 5);
+        Square square1 = new Square("purple", 4);
+        Circle circle1 = new Circle("orange", 4);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        shapes.Add(rectangle1);
+        shapes.Add(square1);
+        shapes.Add(circle1);
+
+        foreach(Shape shape in shapes){
+            Console.WriteLine("Color: \t " + shape.GetColor());
+            Console.WriteLine($"Area: \t {shape.GetArea()}"); 
+        }
     }
 }
